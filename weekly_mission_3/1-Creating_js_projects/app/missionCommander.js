037775f20edd88ac27bf7ab62ad9ea2b1b1a5c74
Kevin Garcia Ramirez <mr.kegara@gmail.com>
2022-04-30 19:25:52 -0500

    creating js project with tests

---
+++ weekly_mission_3/1-Creating_js_projects/app/missionCommander.js
@@ -0,0 +1,7 @@
+class MissionCommander {
+    constructor (name ="Carlo Gilmar") {
+      this.name = name
+    }
+  }
+  // Esta línea nos permite exportar nuestra clase
+module.exports = MissionCommander
